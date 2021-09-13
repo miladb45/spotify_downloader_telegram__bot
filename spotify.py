226fa412577876f67c82d166faf543865f24c40c
freshremix <90133873+freshremix@users.noreply.github.com>
2021-09-13 12:59:20 +0430

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -16,7 +16,7 @@ spotifyy = spotipy.Spotify(
 genius = lyricsgenius.Genius('biZZReO7F98mji5oz3cE0FiIG73Hh07qoXSIzYSGNN3GBsnY-eUrPAVSdJk_0_de')
 
 
-token = '1979157374:AAH0CWHM3W3EZIFqoHXR8Z5w0GtdeQzEcHI'
+token = '1976883813:AAHx8Oau1X0kF58sDH0cQ9scqE5mQC2BeXM'
 bot = telepot.Bot(token)
 
 def DOWNLOADMP3(link,chat_id):
